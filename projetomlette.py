098658ec3f876736c507043a50a45e978bd14546
sfmk07 <151550819+sfmk07@users.noreply.github.com>
2023-12-14 09:58:34 +0100

    sauvegardefinal

--- projetomlette.py
+++ projetomlette.py
@@ -1,5 +1,6 @@
 import time
 
+# Classe Personnage
 class Personnage:
     def __init__(self, nom, lieu, argent):
         self.nom = nom
@@ -9,127 +10,117 @@ class Personnage:
         self.main_gauche = []
 
     def se_deplacer(self, lieu):
-        self.lieu = lieu
+        self.lieu = lieu.nom
         print(f"{self.nom} est actuellement à la {self.lieu}")
 
     def payer_article(self, article):
         self.argent -= article.prix
-        print(f"{self.nom} a payé {article.prix} euros pour {article.nom}. Argent restant : {self.argent} euros")
+        print(f"Il reste {self.argent} euros à {self.nom}")
 
     def couper(self, ingredient, outil):
-        if outil.action == 'entier' and ingredient.etats[0] == 'entier':
-            outil.action = 'coupé'
-            ingredient.etats[0] = 'coupé'
-            print(f"{self.nom} a coupé {ingredient.nom} avec un {outil.nom}")
+        if outil.nom == "Couteau" and ingredient.etat == "entier":
+            ingredient.etat = "coupé"
+            print(f"{self.nom} a coupé {ingredient.nom} avec {outil.nom}")
 
-class Lieu:
-    def __init__(self, nom):
-        self.nom = nom
+# Classe Lieu
+class Maison:
+    def __init__(self):
+        self.nom = 'maison'
         self.personnes = []
 
-class Outil:
-    def __init__(self, nom):
-        self.nom = nom
-        self.action = 'entier'
+class Epicerie:
+    def __init__(self):
+        self.nom = 'épicerie'
+        self.personnes = []
+        self.paniers = [{"type": "panier", "contenu": []}]
+        self.ingredients = []
 
+# Classe Outil
+class Couteau:
+    def __init__(self):
+        self.nom = "Couteau"
+        self.action = "entier"
+
+# Classe Ingrédient
 class Ingredient:
-    def __init__(self, nom, etats, prix):
+    def __init__(self, nom, prix):
         self.nom = nom
-        self.etats = etats
+        self.etat = "entier"
         self.prix = prix
 
-class Epicerie(Lieu):
-    def __init__(self, nom, ingrédients):
-        super().__init__(nom)
-        self.paniers = [{'type': 'panier', 'contenu': []}]
-        self.ingrédients = ingrédients
-
+# Classe Poele
 class Poele:
     def __init__(self):
         self.contenu = []
 
     def cuire(self):
         time.sleep(4)
-        self.contenu[0].etats[0] = 'cuit'
-        print("L'omelette est cuite :)")
+        if self.contenu:
+            self.contenu[0].etat = 'cuit'
+            print("Notre omelette est cuite :)")
 
+# Classe Bol
 class Bol:
     def __init__(self):
         self.contenu = []
 
     def melanger(self, nom_melange):
-        new_melange = Ingredient(nom_melange, ['pas cuit'], 0)
+        new_melange = Ingredient(nom_melange, 0)
+        new_melange.etat = 'pas cuit'
         self.contenu = [new_melange]
-        print(f"{nom_melange} a été mélangé dans le bol.")
+        print(f"{nom_melange} a été mélangé dans le bol")
 
-# Début du scénario
-# Création des ingrédients
-oeuf = Ingredient('œuf', ['entier'], 1.0)
-lait = Ingredient('lait', ['entier'], 0.5)
-sel = Ingredient('sel', ['entier'], 0.2)
-poivre = Ingredient('poivre', ['entier'], 0.3)
+# Exemple de déroulement du scénario
+maison = Maison()
+epicerie = Epicerie()
+personnage = Personnage("Sofiane", "quelque part", 100)
 
-# Création des lieux
-maison = Lieu('maison')
-epicerie = Epicerie('épicerie', [oeuf, lait, sel, poivre])
+# Déplacer le personnage à la maison
+personnage.se_deplacer(maison)
 
-# Création des outils
-couteau = Outil('couteau')
+# Ajouter des ingrédients à l'épicerie pour l'exemple
+epicerie.ingredients.append(Ingredient("Oeuf", 0.5))
+epicerie.ingredients.append(Ingredient("Lait", 1))
+epicerie.ingredients.append(Ingredient("Farine", 0.75))
 
-# Création des objets de préparation
-poele = Poele()
-bol = Bol()
+# Déplacer le personnage à l'épicerie
+personnage.se_deplacer(epicerie)
 
-# Création du personnage
-personnage = Personnage('Sofiane', 'maison', 10.0)
-
-# Indiquer que le personnage est à la maison
-personnage.se_deplacer(maison.nom)
-
-# Aller à l'épicerie acheter les ingrédients
-personnage.se_deplacer(epicerie.nom)
-personnage.main_droite.append(epicerie.paniers[0])
+# Prendre un panier
+panier = epicerie.paniers[0]
+personnage.main_droite.append(panier)
 print(f"{personnage.nom} a pris un panier")
 
-# Boucle pour prendre chaque ingrédient dans l'épicerie
-for ingredient in epicerie.ingrédients:
-    copie_ingredient = Ingredient(ingredient.nom, ingredient.etats, ingredient.prix)
-    personnage.main_droite[0]['contenu'].append(copie_ingredient)
-    print(f"{personnage.nom} a pris {ingredient.nom}")
-    personnage.payer_article(ingredient)
+# Acheter des ingrédients
+for ingredient in epicerie.ingredients:
+    copie_ingredient = Ingredient(ingredient.nom, ingredient.prix)
+    panier["contenu"].append(copie_ingredient)
+    print(f"{personnage.nom} a pris {copie_ingredient.nom}")
+    personnage.payer_article(copie_ingredient)
 
 # Retourner à la maison
-personnage.se_deplacer(maison.nom)
-print(f"{personnage.nom} est de retour à la maison")
+personnage.se_deplacer(maison)
 
-# Mettre chaque ingrédient dans le bol
-for ingredient in personnage.main_droite[0]['contenu']:
+# Mettre les ingrédients dans le bol
+bol = Bol()
+for ingredient in panier["contenu"]:
     bol.contenu.append(ingredient)
-    print(f"{ingredient.nom} a été mis dans le bol")
+    print(f"{ingredient.nom} mis dans le bol")
+panier["contenu"].clear()
 
 # Retourner à l'épicerie pour rapporter le panier
-personnage.se_deplacer(epicerie.nom)
-epicerie.paniers.append(personnage.main_droite.pop())
-print(f"{personnage.nom} a rapporté le panier à l'épicerie")
+personnage.se_deplacer(epicerie)
+personnage.main_droite.remove(panier)
+epicerie.paniers.append(panier)
+print(f"Panier retourné à l'épicerie")
 
-# Retourner à la maison pour continuer l'omelette
-personnage.se_deplacer(maison.nom)
-print(f"{personnage.nom} est de retour à la maison")
+# Retourner à la maison
+personnage.se_deplacer(maison)
 
-# Vérifier chaque ingrédient dans le bol et le couper seulement s'il est entier
+# Couper les ingrédients si nécessaire
+couteau = Couteau()
 for ingredient in bol.contenu:
     personnage.couper(ingredient, couteau)
 
-# Mélanger le contenu du bol pour créer une 'omelette'
-bol.melanger('omelette')
-
-# Vider le contenu du bol dans la poêle
-poele.contenu = bol.contenu
-bol.contenu = []
-print(f"{personnage.nom} a versé le contenu du bol dans la poêle")
-
-# Cuire l'omelette
-poele.cuire()
-
-# Afficher un message final
-print("Notre omelette est cuite :)")
+# Mélanger les ingrédients
+bol.melanger
